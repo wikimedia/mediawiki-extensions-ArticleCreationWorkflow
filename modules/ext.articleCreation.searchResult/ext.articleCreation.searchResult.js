c93fe0cadc38c2a07bd547906c227a42bc8517c2
Paladox <thomasmulhall410@yahoo.com>
2016-03-13 20:03:27 +0000

    Archiving extension
    We should archive ArticleCreationWorkflow instead. It was solely for A/B
    testing and depended on ClickTracking extension which is gone afaik.
    
    Ref: https://www.mediawiki.org/wiki/Extension:ArticleCreationWorkflow
    
    Change-Id: I9320997628e9ba28eda203309ba052f17ac77b9e

--- modules/ext.articleCreation.searchResult/ext.articleCreation.searchResult.js
+++
@@ -1,9 +0,0 @@
-jQuery( document ).ready( function() {
-	var newTitle = 'Special:ArticleCreationLanding' + '/' +
-		encodeURIComponent(mw.config.get('acSearch'));
-	var landingURL = mw.config.get('wgArticlePath').replace( '$1', newTitle );
-	// change the link to point to the new special page
-	jQuery("div.searchresults")
-		.find('a[href*="action=edit"]')
-		.attr( 'href', landingURL );
-} );
