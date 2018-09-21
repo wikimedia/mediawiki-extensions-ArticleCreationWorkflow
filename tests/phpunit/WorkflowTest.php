61e3d286c1412cf8c94602410374dc36ecdba533
Max Semenik <maxsem.wiki@gmail.com>
2018-09-21 16:50:42 -0700

    Truly prevent page creation, add a permission
    Adds a new user permission, by default given to all users,
    to create mainspace pages. If withdrawn, prevents creation properly
    instead of just hijacking the editing interface.
    
    Due to changes in configuration, needs a mediawiki-config change
    before merging.
    
    Bug: T204016
    Change-Id: Ibe04a3ad01cfadd903eff58faa33913e4c5167e0

--- tests/phpunit/WorkflowTest.php
+++ tests/phpunit/WorkflowTest.php
@@ -2,72 +2,117 @@
 
 namespace ArticleCreationWorkflow\Tests;
 
-use Article;
 use ArticleCreationWorkflow\Workflow;
 use DerivativeContext;
 use HashConfig;
 use MediaWikiTestCase;
+use OutputPage;
 use RequestContext;
 use Title;
 use User;
 
 /**
- * @covers \ArticleCreationWorkflow\Workflow
  * @group ArticleCreationWorkflow
  */
 class WorkflowTest extends MediaWikiTestCase {
+	const REDIRECT_URL = 'this is a URL, trust me';
 
 	/**
-	 * @dataProvider provideShouldInterceptEditPage
+	 * @dataProvider providePageInterception
+	 *
+	 * @covers \ArticleCreationWorkflow\Workflow::shouldInterceptPage()
 	 *
 	 * @param User $user
 	 * @param Title $title
-	 * @param array $settings
 	 * @param bool $expected
 	 */
-	public function testShouldInterceptEditPage( User $user, Title $title,
-		$settings, $expected
-	) {
+	public function testShouldInterceptPage( User $user, Title $title, $expected ) {
 		$context = new DerivativeContext( RequestContext::getMain() );
 		$context->setTitle( $title );
 		$context->setUser( $user );
 
-		$article = new Article( $title );
-		$article->setContext( $context );
-		$config = new HashConfig( [ 'ArticleCreationWorkflows' => $settings ] );
+		$config = new HashConfig();
 
 		$landingPage = $this->getMock( Title::class, [ 'exists' ] );
 		$landingPage->method( 'exists' )->willReturn( true );
 		$workflow = $this->getMock( Workflow::class, [ 'getLandingPageTitle' ], [ $config ] );
 		$workflow->method( 'getLandingPageTitle' )->willReturn( $landingPage );
 
-		self::assertEquals( $expected, $workflow->shouldInterceptEditPage( $article, $user ) );
+		/** @var Workflow $workflow */
+		self::assertEquals( $expected, $workflow->shouldInterceptPage( $title, $user ) );
 	}
 
-	public function provideShouldInterceptEditPage() {
+	/**
+	 * @dataProvider providePageInterception
+	 *
+	 * @covers \ArticleCreationWorkflow\Workflow::interceptIfNeeded()
+	 *
+	 * @param User $user
+	 * @param Title $title
+	 * @param bool $expected
+	 */
+	public function testInterceptIfNeeded( User $user, Title $title, $expected ) {
+		$output = $this->getMockBuilder( OutputPage::class )
+			->disableOriginalConstructor()
+			->setMethods( [ 'redirect' ] )
+			->getMock();
+
+		if ( $expected ) {
+			$output->expects( self::once() )
+				->method( 'redirect' )
+				->with( self::REDIRECT_URL );
+		} else {
+			$output->expects( self::never() )
+				->method( 'redirect' );
+		}
+
+		$context = new DerivativeContext( RequestContext::getMain() );
+		$context->setTitle( $title );
+		$context->setUser( $user );
+		/** @var OutputPage $output */
+		$context->setOutput( $output );
+
+		$config = new HashConfig();
+
+		$landingPage = $this->getMock( Title::class, [ 'exists', 'getFullURL' ] );
+		$landingPage->method( 'exists' )->willReturn( true );
+		$landingPage->method( 'getFullURL' )
+			->with( [ 'page' => $title->getPrefixedText() ] )
+			->willReturn( self::REDIRECT_URL );
+		$workflow = $this->getMock( Workflow::class, [ 'getLandingPageTitle' ], [ $config ] );
+		$workflow->method( 'getLandingPageTitle' )->willReturn( $landingPage );
+
+		/** @var Workflow $workflow */
+		self::assertEquals( $expected, $workflow->interceptIfNeeded( $title, $user, $context ) );
+	}
+
+	public function providePageInterception() {
 		$anon = $this->getMock( 'User' );
 		$anonmap = [
 			[ 'autoconfirmed', false ],
-			[ 'createpage', false ]
+			[ 'createpage', false ],
+			[ 'createpagemainns', false ],
 		];
 		$anon->method( 'isAllowed' )
-			->will( $this->returnValueMap( $anonmap ) );
+			->will( self::returnValueMap( $anonmap ) );
 
 		$newbie = $this->getMock( 'User' );
 		$newbiemap = [
 			[ 'autoconfirmed', false ],
-			[ 'createpage', true ]
+			[ 'createpage', true ],
+			[ 'createpagemainns', false ],
 		];
 		$newbie->method( 'isAllowed' )
-			->will( $this->returnValueMap( $newbiemap ) );
+			->will( self::returnValueMap( $newbiemap ) );
 
 		$confirmed = $this->getMock( 'User' );
 		$confirmedmap = [
 			[ 'autoconfirmed', true ],
-			[ 'createpage', true ]
+			[ 'createpage', true ],
+			[ 'createpagemainns', true ],
 		];
 		$confirmed->method( 'isAllowed' )
-			->will( $this->returnValueMap( $confirmedmap ) );
+			->will( self::returnValueMap( $confirmedmap ) );
 
 		$mainspacePage = Title::newFromText( 'Some nonexistent page' );
 		$miscPage = Title::newFromText( 'Project:Nonexistent too' );
@@ -77,45 +122,38 @@ class WorkflowTest extends MediaWikiTestCase {
 		$existingPage->method( 'getContentModel' )
 			->willReturn( CONTENT_MODEL_WIKITEXT );
 
-		$config = [
-			[
-				'namespaces' => [ NS_MAIN ],
-				'excludeRight' => 'autoconfirmed',
-			],
-		];
-
 		return [
-			[ $anon, $mainspacePage, [], false, 'No config, do nothing' ],
-			[ $anon, $miscPage, $config, false, 'Wrong NS, do nothing' ],
-			[ $anon, $existingPage, $config, false, 'Page exists, do nothing' ],
-			[ $anon, $mainspacePage, $config, false, 'Anon attempting to create a page, do nothing' ],
-			[ $confirmed, $mainspacePage, $config, false, 'Confirmed user, do nothing' ],
-
-			[ $newbie, $mainspacePage, $config, true, 'Newbie attempting to create a page, intercept' ],
+			[ $anon, $miscPage, false, 'Wrong NS, do nothing' ],
+			[ $anon, $existingPage, false, 'Page exists, do nothing' ],
+			[ $anon, $mainspacePage, false, 'Anon attempting to create a page, do nothing' ],
+			[ $confirmed, $mainspacePage, false, 'Confirmed user in mainspace, do nothing' ],
+			[ $confirmed, $existingPage, false, 'Confirmed user on an existing page, do nothing' ],
+			[ $confirmed, $miscPage, false, 'Confirmed user not in mainspace, do nothing' ],
+			[ $newbie, $mainspacePage, true, 'Newbie attempting to create a page, intercept' ],
+			[ $newbie, $existingPage, false, 'Newbie on an existing page, do nothing' ],
+			[ $newbie, $miscPage, false, 'Newbie attempting to create a non-mainspace page, do nothing' ],
 		];
 	}
 
+	/**
+	 * @covers \ArticleCreationWorkflow\Workflow::shouldInterceptPage()
+	 */
 	public function testLandingPageExistence() {
-		$article = new Article( Title::newFromText( 'Test page' ) );
+		$title = Title::newFromText( 'Test page' );
 		$user = $this->getMock( 'User' );
 		$user->method( 'isAllowed' )
-			->with( 'createpage' )
-			->willReturn( true );
+			->will( self::returnValue( true ) );
 		$config = new HashConfig( [
-			'ArticleCreationWorkflows' => [
-				[
-					'namespaces' => [ NS_MAIN ],
-					'excludeRight' => 'autoconfirmed',
-				],
-			],
-			'ArticleCreationLandingPage' => 'Non existant page',
+			'ArticleCreationLandingPage' => 'Nonexistent page',
 		] );
 
 		$workflow = $this->getMock( Workflow::class, [ 'getLandingPageTitle' ], [ $config ] );
 		$workflow->method( 'getLandingPageTitle' )->willReturn( null );
 
 		// Check that it doesn't intercept if the message is empty
-		self::assertEquals( false, $workflow->shouldInterceptEditPage( $article, $user ) );
+		/** @var Workflow $workflow */
+		/** @var User $user */
+		self::assertEquals( false, $workflow->shouldInterceptPage( $title, $user ) );
 	}
 
 }
